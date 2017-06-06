d38e3f82cd27ad2073e24e13d8c7cdf05ec9b9a9
arthurxavier <arthurxaviergiffoni@gmail.com>
2017-06-06 18:03:13 +0000

    Config the routes file for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json }, constraints: {subdomain: 'api'}, path: "/" do
+    
+  end
+
+  #Controlers de um admin, contruir pasta no controllers, admin
+  # namespace :admin do
+  # end
 end
